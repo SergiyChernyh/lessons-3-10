ed5cab9c35778cc165dd0e66c7546c31606bd6b5
SergiyChernyh <alterdawon@gmail.com>
2023-11-05 20:49:39 +0200

    home 5

---
+++ home_work_5.py
@@ -0,0 +1,21 @@
+##################################################
+# my_list = [12, 37, 99, 204, 304, 21]
+# for list in my_list:
+#     if list > 100:
+#         print(list)
+
+###################################################################
+# my_list = [12, 37, 99, 204, 304, 21]
+# my_result = []
+# for list in my_list:
+#     if list > 100:
+#      my_result.append(list)
+# print(my_result)
+#################################################################
+# my_list = [12, 25, 99, 204, 12, 3]
+# n = len(my_list)
+# if n < 2:
+#  my_list.append(0)
+# elif n >= 2:
+#    s = my_list[-1] + my_list[-2]
+# print(s)
